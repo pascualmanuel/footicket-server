3c68961c801edd624bef15ddda21b230e1ba4f18
pascualmanuel <manuelnpascual@gmail.com>
2023-02-02 12:17:22 -0300

    Fixing Cors

--- config/index.js
+++ config/index.js
@@ -35,7 +35,11 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
+      origin: [
+        process.env.ORIGIN,
+        "http://localhost:3000",
+        "https://footicket-server.onrender.com/",
+      ],
     })
   );
 
