e27bb9bdcb93d7b2fb819a5827c37e64d788428f
pascualmanuel <manuelnpascual@gmail.com>
2023-02-03 12:35:51 -0300

    Module export

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const app = require("./app");
-
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
-const app = express();
-
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://footicket.onrender.com/"
-  );
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://footicket.onrender.com/",
+    "https://footicket-server.onrender.com/",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
 
