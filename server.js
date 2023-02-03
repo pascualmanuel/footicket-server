731204b85319ce9494a45b67d03a137644d3f1b4
pascualmanuel <manuelnpascual@gmail.com>
2023-02-03 13:23:13 -0300

    Yanosequehacer

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const PORT = process.env.PORT || 5005;
 // Add Access Control Allow Origin headers
 app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://footicket.onrender.com/",
-    "https://footicket-server.onrender.com/",
-  ];
+  const allowedOrigins = ["https://footicket-server.onrender.com/"];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
