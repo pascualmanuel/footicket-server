ee56b30e1e555e4813a6ad7fadd9334e7c6f60c1
pascualmanuel <manuelnpascual@gmail.com>
2023-02-03 12:24:56 -0300

    Server cors

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
+const app = express();
+const port = process.env.SERVER_PORT || 8000;
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://footicket.onrender.com/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
